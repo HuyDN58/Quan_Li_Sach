3151db80e678cb5ad590e9f60c8a21062d5ef691
quannh20 <quantaday2003@gmail.com>
2023-05-21 01:04:04 +0700

    CRUD BANG KH&Account

---
+++ AppAPI/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AppData.IRepositories;
+using AppData.Repositories;
+using AppData.Models;
+using System.Drawing;
+
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private IAllRepositories<Account> irepos;
+        private QUAN_LI_SACH_NET105Context context= new QUAN_LI_SACH_NET105Context();
+        public AccountController()
+        {
+            AllRepositories<Account> repos = new AllRepositories<Account>(context,context.Accounts);
+            irepos = repos;
+        }
+        [HttpGet]
+        public IEnumerable<Account> Get()
+        {
+            return irepos.GetAll();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public Account Get(Guid id)
+        {
+            return irepos.GetAll().First(c => c.IdnguoiDung == id);
+        }
+        [HttpPost("Create-mausac")]
+        public bool CreateColor(string taiKhoan, string matKhau)
+        {
+            Account acc = new Account();
+            acc.TaiKhoan = taiKhoan; acc.MatKhau = matKhau; acc.IdnguoiDung = Guid.NewGuid();
+            return irepos.CreateItem(acc);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public bool Update(Guid id, string taiKhoan, string matKhau)
+        {
+            var acc = irepos.GetAll().First(c => c.IdnguoiDung == id);
+            acc.TaiKhoan = taiKhoan; acc.MatKhau = matKhau; ;
+            return irepos.UpdateItem(acc);
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(Guid id)
+        {
+            var acc = context.Accounts.Find(id);
+            return irepos.DeleteItem(acc);
+        }
+    }
+}
---
+++ AppAPI/Controllers/KhachHangController.cs
@@ -0,0 +1,58 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KhachHangController : ControllerBase
+    {
+        private IAllRepositories<KhachHang> irepos;
+        private QUAN_LI_SACH_NET105Context context = new QUAN_LI_SACH_NET105Context();
+        public KhachHangController()
+        {
+            AllRepositories<KhachHang> repos = new AllRepositories<KhachHang>(context, context.KhachHangs);
+            irepos = repos;
+        }
+        [HttpGet]
+        public IEnumerable<KhachHang> Get()
+        {
+            return irepos.GetAll();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public KhachHang Get(Guid id)
+        {
+            return irepos.GetAll().First(c => c.Idkh == id);
+        }
+        [HttpPost("Create-mausac")]
+        public bool CreateColor(string ma, string tenkh, Guid idacc, string sdt, string diachi, int trangthai)
+        {
+            KhachHang kh = new KhachHang();
+            kh.Ma = ma; kh.TenKh = tenkh; kh.IdnguoiDung = idacc; kh.Sdt = sdt;
+            kh.DiaChi = diachi; kh.TrangThai = trangthai; kh.Idkh = Guid.NewGuid();
+            return irepos.CreateItem(kh);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public bool Update(Guid id, string ma, string tenkh, Guid idacc, string sdt, string diachi, int trangthai)
+        {
+            var kh = irepos.GetAll().First(c => c.Idkh == id);
+            kh.Ma = ma; kh.TenKh = tenkh; kh.IdnguoiDung = idacc; kh.Sdt = sdt;
+            kh.DiaChi = diachi; kh.TrangThai = trangthai;
+            return irepos.UpdateItem(kh);
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(Guid id)
+        {
+            var kh = context.KhachHangs.Find(id);
+            return irepos.DeleteItem(kh);
+        }
+    }
+}
