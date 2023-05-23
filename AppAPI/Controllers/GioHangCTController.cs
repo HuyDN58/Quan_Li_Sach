22ae8480735ae7c2fba2f301c1336b674c0737c6
huydn <dohuy9084@gmail.com>
2023-05-24 00:01:02 +0700

    CRUD

---
+++ AppAPI/Controllers/GioHangCTController.cs
@@ -0,0 +1,57 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GioHangCTController : ControllerBase
+    {
+        private IAllRepositories<GioHangCt> irepos;
+        private QUAN_LI_SACH_NET105Context context = new QUAN_LI_SACH_NET105Context();
+        public GioHangCTController()
+        {
+            AllRepositories<GioHangCt> repos = new AllRepositories<GioHangCt>(context, context.GioHangCts);
+            irepos = repos;
+        }
+        [HttpGet]
+        public IEnumerable<GioHangCt> Get()
+        {
+            return irepos.GetAll();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public GioHangCt Get(Guid id)
+        {
+            return irepos.GetAll().First(c => c.Idghct == id);
+        }
+        [HttpPost("Create")]
+        public bool Create(int soluong, int trangthai, Guid idgh, Guid idspct)
+        {
+            GioHangCt acc = new GioHangCt();
+            acc.SoLuong = soluong; acc.TrangThai = trangthai; acc.Idspct = idspct; acc.Idgh = idgh; acc.Idghct = Guid.NewGuid();
+            return irepos.CreateItem(acc);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public bool Update(Guid id, int soluong, int trangthai, Guid idgh, Guid idspct)
+        {
+
+            var acc = irepos.GetAll().First(c => c.Idghct == id);
+            acc.SoLuong = soluong; acc.TrangThai = trangthai; acc.Idspct = idspct; acc.Idgh = idgh;
+            return irepos.UpdateItem(acc);
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(Guid id)
+        {
+            var acc = context.GioHangCts.Find(id);
+            return irepos.DeleteItem(acc);
+        }
+    }
+}
---
+++ AppView/Controllers/TacGiaController.cs
@@ -0,0 +1,27 @@
+﻿using AppData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AppView.Controllers
+{
+    public class TacGiaController : Controller
+    {
+        public TacGiaController()
+        {
+
+        }
+        public async Task<IActionResult> ShowAll()
+        {
+            // Call API
+            string apiUrl = "https://localhost:7273/api/TacGia";
+            var httpClient = new HttpClient(); // tạo ra để callApi
+            var response = await httpClient.GetAsync(apiUrl);// Lấy dữ liệu ra từ API URL
+                                                             // Lấy dữ liệu Json trả về từ Api được call dạng string
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kqua trả về từ API dạng Json
+            // Đọc từ string Json vừa thu được sang double
+            var colors = JsonConvert.DeserializeObject<List<TacGium>>(apiData);
+            return View(colors);
+        }
+    }
+}
