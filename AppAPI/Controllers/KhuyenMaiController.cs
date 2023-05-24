3a543a71123ca1546543d0ac56731228c78e53e5
choem <choem@NGOCSINH>
2023-05-24 15:21:30 +0700

    Duy

---
+++ AppAPI/Controllers/KhuyenMaiController.cs
@@ -0,0 +1,58 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KhuyenMaiController : ControllerBase
+    {
+        private IAllRepositories<KhuyenMai> irepos;
+        private QUAN_LI_SACH_NET105Context context = new QUAN_LI_SACH_NET105Context();
+        public KhuyenMaiController()
+        {
+            AllRepositories<KhuyenMai> repos =
+                new AllRepositories<KhuyenMai>(context, context.KhuyenMais);
+            irepos = repos;
+        }
+        [HttpGet]
+        public IEnumerable<KhuyenMai> Get()
+        {
+            return irepos.GetAll();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public KhuyenMai Get(Guid id)
+        {
+            return irepos.GetAll().First(c => c.Idkm == id);
+        }
+        [HttpPost("Create")]
+        public bool Create(string MaKm, DateTime NgayBd, DateTime NgayKt, int PhanTramGiam, int? trangthai)
+        {
+            KhuyenMai promtion = new KhuyenMai();
+            promtion.MaKm = MaKm; promtion.NgayBd = NgayBd; promtion.NgayKt = NgayKt; promtion.PhanTramGiam = PhanTramGiam; promtion.TrangThai = trangthai;
+            return irepos.CreateItem(promtion);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public bool Update(Guid Idkm, string MaKm, DateTime NgayBd, DateTime NgayKt, int PhanTramGiam, int trangthai)
+        {
+
+            var promotion = irepos.GetAll().First(c => c.Idkm == Idkm);
+            promotion.Idkm = Idkm; promotion.MaKm = MaKm; promotion.NgayBd = NgayBd; promotion.NgayKt = NgayKt; promotion.PhanTramGiam = PhanTramGiam; promotion.TrangThai = trangthai;
+            return irepos.UpdateItem(promotion);
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(Guid id)
+        {
+            var promotion = context.KhuyenMais.Find(id);
+            return irepos.DeleteItem(promotion);
+        }
+    }
+}
---
+++ AppView/Controllers/KhuyenMaiController.cs
@@ -0,0 +1,28 @@
+﻿using AppData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AppView.Controllers
+{
+    public class KhuyenMaiController : Controller
+    {
+        public KhuyenMaiController()
+        {
+            
+        }
+        public async Task<IActionResult> ShowAllPromotion()
+        {
+            // Call API
+            string apiUrl = "https://localhost:7273/api/KhuyenMai";
+            var httpClient = new HttpClient(); // tạo ra để callApi
+            var response = await httpClient.GetAsync(apiUrl);// Lấy dữ liệu ra từ API URL
+                                                             // Lấy dữ liệu Json trả về từ Api được call dạng string
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kqua trả về từ API dạng Json
+            // Đọc từ string Json vừa thu được sang double
+            var promotion = JsonConvert.DeserializeObject<List<KhuyenMai>>(apiData);
+            return View(promotion);
+        }
+
+    }
+}
